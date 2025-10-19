6295571aefe2ff51e37dd6f039d98a319ea8f38f
Unterseiter <wwwandromedix@gmail.com>
2025-10-19 22:17:42 +0300

    start work on main-page

--- invest-portfolio-frontend/src/pages/main/HomePage.jsx
+++ invest-portfolio-frontend/src/pages/main/HomePage.jsx
@@ -1,8 +1,25 @@
+import './Main-page.css'
+
 function MainPage () {
     return (
-        <div>
-            main-page
+        <div className='portfolio-review-box'>
+            
+            <div className="portfolio-review">
+                <h1>
+                    Обзор портфеля
+                </h1>
+                <p>
+                    Общая информация о ваших инвестициях и текущем состоянии портфеля
+                </p>
+            </div>
+
+            <div className="portfolio-info">
+                {/* ячейка общей стоимости */} <div className="total-cost">$</div>
+                {/* кол-во активов */} <div className="number-actives">**</div>
+                {/* Лучший актив */} <div className="best-active">active</div>
+            </div>
         </div>
+
     )
 }
 
